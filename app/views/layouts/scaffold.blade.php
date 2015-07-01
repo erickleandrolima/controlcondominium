e53d7a5d98547c56f54353a623431a69bc9f3665
erick leandro <erick.leandro.lima@hotmail.com>
2015-06-30 22:00:40 -0400

    in this commit
    - added base tag in header
    - added meta tag viewport in header

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -2,13 +2,15 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/assets/css/bootstrap.css">
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/jquery-ui.css" />
-    <script src="/assets/js/jquery-2.1.3.min.js"></script>
-    <script src="/assets/js/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/assets/js/jquery.mask.min.js"></script>
-    <script type="text/javascript" src="/assets/js/jquery.quicksearch.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="http://localhost/condominio/public/"></base>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css" />
+    <script src="assets/js/jquery-2.1.3.min.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.quicksearch.js"></script>
     <script type="text/javascript">
 
       $(function(){
@@ -53,13 +55,13 @@
   <div class="container-fluid">
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="/months"><?php echo Lang::get('months.title') ?> </a></li>
-        <li><a href="/dwellers"><?php echo Lang::get('dwellers.title') ?></a></li>
-        <li><a href="/expenses"><?php echo Lang::get('expenses.title') ?></a></li>
-        <li><a href="/categories"><?php echo Lang::get('categories.title') ?></a></li>
-        <li><a href="/reports"> <?php echo Lang::get('reports.title') ?> </a></li>
+        <li><a href="months"><?php echo Lang::get('months.title') ?> </a></li>
+        <li><a href="dwellers"><?php echo Lang::get('dwellers.title') ?></a></li>
+        <li><a href="expenses"><?php echo Lang::get('expenses.title') ?></a></li>
+        <li><a href="categories"><?php echo Lang::get('categories.title') ?></a></li>
+        <li><a href="reports"> <?php echo Lang::get('reports.title') ?> </a></li>
         @if(Auth::check())
-          <li><a href="/users/logout"><?php echo Lang::get('login.logout') ?></a></li>
+          <li><a href="users/logout"><?php echo Lang::get('login.logout') ?></a></li>
         @endif
       </ul>
     </div>  <!-- collapse navbar -->
