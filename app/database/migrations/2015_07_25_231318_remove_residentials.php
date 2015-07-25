8165e27fd29c150dee011ecaa6cdafd3f57b4b60
erick leandro <erick.leandro.lima@hotmail.com>
2015-07-25 19:15:18 -0400

    in this commit:
    - removed residentials module because this feature will be implemented
    in other version this software

--- app/database/migrations/2015_07_25_231318_remove_residentials.php
+++ app/database/migrations/2015_07_25_231318_remove_residentials.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusForUsersTable extends Migration {
+class RemoveResidentials extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,9 +12,7 @@ class AddStatusForUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('users', function(Blueprint $table) {
-     		$table->integer('situation')->default(0);
-     	});
+		Schema::drop('residentials');
 	}
 
 	/**
@@ -24,9 +22,7 @@ class AddStatusForUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::table('users', function(Blueprint $table) {
-     		$table->dropColumn('situation');
-     	});
+		//
 	}
 
 }
